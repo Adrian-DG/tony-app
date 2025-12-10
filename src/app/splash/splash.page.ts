1f4c81068eb6cfe0eefa34e65a760d2a12bab67d
Adrian-DG <adrian96guilamo@gmail.com>
2025-12-10 15:41:02 -0400

    Implement splash screen with conditional call-to-action; refactor styles and add animation

--- src/app/splash/splash.page.ts
+++ src/app/splash/splash.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
 	selector: 'app-splash',
-	imports: [IonicModule],
+	imports: [IonicModule, RouterModule],
 	templateUrl: './splash.page.html',
 	styleUrls: ['./splash.page.scss'],
 })
-export class SplashPage {
+export class SplashPage implements OnInit {
+	isActionVisible$ = signal<boolean>(false);
 	constructor() {}
+	ngOnInit(): void {
+		setTimeout(() => {
+			this.isActionVisible$.set(true);
+		}, 3000);
+	}
 }
