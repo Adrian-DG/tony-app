04faa4c44c34cd18494d9e46288f12b5a66e2dcc
Adrian-DG <adrian96guilamo@gmail.com>
2025-12-11 16:24:50 -0400

    Add Maskito integration for input masking; update login form fields and styles

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -6,20 +6,53 @@ import {
 	Validators,
 } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import {
+	MaskitoOptions,
+	MaskitoElementPredicate,
+	maskitoTransform,
+} from '@maskito/core';
+import { MaskitoDirective } from '@maskito/angular';
 
 @Component({
 	selector: 'app-login',
-	imports: [IonicModule, ReactiveFormsModule],
+	imports: [IonicModule, ReactiveFormsModule, MaskitoDirective],
 	templateUrl: './login.page.html',
 	styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
 	loginForm: FormGroup;
 
+	readonly cedulaMask: MaskitoOptions = {
+		mask: [
+			/\d/,
+			/\d/,
+			/\d/,
+			'-',
+			/\d/,
+			/\d/,
+			/\d/,
+			/\d/,
+			/\d/,
+			/\d/,
+			/\d/,
+			'-',
+			/\d/,
+		],
+	};
+
+	cedulaMaskedValue = maskitoTransform('00000000000', this.cedulaMask);
+
+	// En login.page.ts
+	readonly cedulaPredicate: MaskitoElementPredicate = async (element) => {
+		// Use querySelector to directly find the native input element.
+		// This is a common and necessary pattern for Maskito with Ionic's Web Components.
+		return element.querySelector('input')!;
+	};
+
 	constructor(private fb: FormBuilder) {
 		this.loginForm = this.fb.group({
-			username: ['', Validators.required],
-			password: ['', Validators.required],
+			cedula: ['', [Validators.required]],
+			password: ['', [Validators.required]],
 		});
 	}
 
