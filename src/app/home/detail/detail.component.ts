0f97769df2a35d53303c8de31d486ea4cb1bbf80
Adrian-DG <adrian96guilamo@gmail.com>
2025-12-21 13:04:37 -0400

    Refactor detail component; update layout and add back button for improved navigation Refactor login page; change button text and toolbar color for consistency Refactor splash page; update call-to-action text and button for better user engagement Refactor splash page styles; enhance typography for call-to-action elements

--- src/app/home/detail/detail.component.ts
+++ src/app/home/detail/detail.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
 	selector: 'app-detail',
 	templateUrl: './detail.component.html',
 	styleUrls: ['./detail.component.scss'],
+	imports: [IonicModule],
 })
 export class DetailComponent implements OnInit {
 	groupId!: number;
