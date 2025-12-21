d43cd44c4efdee652eea9a15297115424a923a3b
Adrian-DG <adrian96guilamo@gmail.com>
2025-12-21 12:50:30 -0400

    Refactor authentication guard; update isAuthenticated check to use async/await for improved handling

--- src/app/core/guards/authentication.guard.ts
+++ src/app/core/guards/authentication.guard.ts
@@ -2,9 +2,9 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { UserService } from '../services/user.service';
 
-export const authenticationGuard: CanActivateFn = () => {
+export const authenticationGuard: CanActivateFn = async () => {
 	const authService = inject(UserService);
-	const isAuthenticated = authService.isAuthenticated$();
+	const isAuthenticated = await authService.isAuthenticated$();
 	if (!isAuthenticated) {
 		authService.redirectToLogin();
 		return false;
