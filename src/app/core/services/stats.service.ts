b38bea9d6d7df67031e94a7058e08fa7f9d4f224
Adrian-DG <adrian96guilamo@gmail.com>
2025-12-27 11:39:43 -0400

    Refactor StatsService to implement getGroupsByCity and getMembersByGroup methods for API calls

--- src/app/core/services/stats.service.ts
+++ src/app/core/services/stats.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { GenericService } from './generic.service';
+import { IBaseStatModel } from '../models/ibase-stat.model';
 
 @Injectable({
 	providedIn: 'root',
@@ -14,27 +15,15 @@ export class StatsService extends GenericService {
 		super($http);
 	}
 
-	// @Get('groups-by-city')
-	// @ApiOperation({ summary: 'Get groups count grouped by city' })
-	// @UseGuards(AuthGuard)
-	// @ApiBearerAuth()
-	// async getGroupsByCity(@Req() request) {
-	//   const userId = request['userId'];
-	//   const userRole = request['role'];
-	//   const result = await this.statsService.getGroupsByCity(userId, userRole);
-	//   return result;
-	// }
-
-	// @Get('members-by-group')
-	// @ApiOperation({ summary: 'Get members count grouped by group' })
-	// async getMembersByGroup(@Req() request) {
-	//   const userId = request['userId'];
-	//   const userRole = request['role'];
-	//   const result = await this.statsService.getMembersByGroup(userId, userRole);
-	//   return result;
-	// }
-
 	getGroupsByCity() {
-		return;
+		return this.$http.get<IBaseStatModel[]>(
+			`${this.apiUrl}/groups-by-city`
+		);
+	}
+
+	getMembersByGroup() {
+		return this.$http.get<IBaseStatModel[]>(
+			`${this.apiUrl}/members-by-group`
+		);
 	}
 }
